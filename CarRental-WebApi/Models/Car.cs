c6bb93be8d048605f270808a2bed273ea827077d
Kacper <kacperrymkiewicz@gmail.com>
2023-10-14 22:10:52 +0200

    Add Customer and Car model

--- CarRental-WebApi/Models/Car.cs
+++ CarRental-WebApi/Models/Car.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CarRental_WebApi.Models
 {
     public class Car
     {
         public int Id { get; set; }
         public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public string RegistrationPlate { get; set; }
+        public int ProductionYear { get; set; }
+        public decimal Price { get; set; }
     }
 }
