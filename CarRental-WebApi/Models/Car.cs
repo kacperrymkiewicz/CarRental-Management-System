e0481a1f0ff800cde7219a03ddd9f44957819434
Kacper <kacperrymkiewicz@gmail.com>
2023-10-14 18:13:47 +0200

    Create initial migration

---
+++ CarRental-WebApi/Models/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRental_WebApi.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+    }
+}
