92e95625debdcd5e122f8f2baf53ad777146e8fb
Kacper <kacperrymkiewicz@gmail.com>
2023-11-18 15:42:24 +0100

    Add login view

---
+++ CarRental-Frontend/src/components/icons/IconArrowRight.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1.66663 6H12.5M8.33329 1L12.744 5.41074C13.0695 5.73618 13.0695 6.26382 12.744 6.58926L8.33329 11" stroke="#F8F9FB" stroke-width="1.5" stroke-linecap="round"/>
+  </svg>
+</template>
---
+++ CarRental-Frontend/src/views/Login.vue
@@ -0,0 +1,88 @@
+<template>
+  <section id="login">
+    <div class="container">
+      <div class="row d-flex justify-content-center">
+        <div class="inner-container col-sm-10 col-md-8 col-lg-6">
+          <h1>Logowanie</h1>
+          <form>
+            <div class="form-group d-flex flex-column align-items-center">
+              <label for="email">Adres email</label>
+              <input type="email" class="form-control" id="email">
+            </div>
+
+            <div class="form-group d-flex flex-column align-items-center">
+              <label for="password">Hasło</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+            <base-button type="dark" :has-icon="true">Zaloguj się</base-button>
+            <p>Nie masz konta? <router-link to="/rejestracja">Zarejestruj się</router-link></p>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.row {
+  p {
+    color: $button-dark;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 22px;
+    letter-spacing: -0.001em;
+    color: $secondary;
+
+    a {
+      color: $teal;
+    }
+  }
+}
+
+div.inner-container {
+  form {
+    div.form-group {
+      margin: 13px 0;
+
+      label {
+        font-weight: 500;
+        font-size: 14px;
+        line-height: 30px;
+        letter-spacing: -0.001em;
+        color: $secondary;
+        align-self: flex-start;
+      }
+
+      input {
+        box-sizing: border-box;
+        border: 1px solid #5F6D7E;
+        border-radius: 8px;
+        display: flex;
+        padding: 8px 18px;
+        gap: 16px;
+        background: #F8F9FB;
+      }
+    }
+
+  }
+
+  h1 {
+    margin: 60px 0;
+    font-weight: 700;
+    font-weight: 700;
+    font-size: 52px;
+    line-height: 60px;
+    text-align: center;
+    letter-spacing: -0.01em;
+    color: $button-dark;
+  }
+
+  div+p {
+    margin: 0;
+    color: $button-red;
+  }
+
+}
+</style>
+  
