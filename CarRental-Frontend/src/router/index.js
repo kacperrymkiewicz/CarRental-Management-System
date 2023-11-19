b463082fca64f0c70103f47511fa9069aa6fe622
Kacper <kacperrymkiewicz@gmail.com>
2023-11-19 01:11:09 +0100

    Scroll to top on route change

--- CarRental-Frontend/src/router/index.js
+++ CarRental-Frontend/src/router/index.js
@@ -1,34 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
+
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/logowanie',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/samochody',
+    name: 'cars',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/logowanie',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/samochody',
-      name: 'cars',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+  scrollBehavior() {
+    return { top: 0, left: 0, /*behavior: "instant"*/ }
+  }
+});
 
-export default router
+export default router;
