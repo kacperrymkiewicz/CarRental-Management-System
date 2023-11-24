23ba248e38f6823b953bc954eaf88172ec641d25
Kacper <kacperrymkiewicz@gmail.com>
2023-11-24 19:14:44 +0100

    Update cors policy

--- CarRental-WebApi/Program.cs
+++ CarRental-WebApi/Program.cs
@@ -53,6 +53,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .SetIsOriginAllowed(origin => true));
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
