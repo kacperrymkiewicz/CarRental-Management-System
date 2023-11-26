efe78329b274c351a12932086f2e64133019ceaa
Kacper <kacperrymkiewicz@gmail.com>
2023-11-26 03:03:26 +0100

    Add new dropdown arrow icon

---
+++ CarRental-Frontend/src/components/icons/IconDropdownMenuArrowDown.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M11 1.33331L6.58926 5.74406C6.26382 6.0695 5.73618 6.0695 5.41074 5.74406L1 1.33331" stroke="#5F6D7E" stroke-width="1.5" stroke-linecap="round"/>
+  </svg>
+</template>
