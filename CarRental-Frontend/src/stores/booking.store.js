3c618215824a644902dfe54a99cee0b1c28fa6e4
Kacper <kacperrymkiewicz@gmail.com>
2023-12-01 17:57:18 +0100

    Add booking store

---
+++ CarRental-Frontend/src/stores/booking.store.js
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia'
+import axios from 'axios';
+
+export const useBookingStore = defineStore('booking', {
+  state: () => ({
+    searchResult: [],
+  }),
+  getters: {
+    carsFound: (state) => state.searchResult.length > 0,
+  },
+  actions: {
+    async searchCars(pickupDate, returnDate) {
+      const responseStatus = { success: null, message: null }
+
+      await axios.get('/Cars/Availability', {
+        pickupDate: pickupDate,
+        returnDate: returnDate,
+      })
+      .then((response) => {
+        this.searchResult = response.data.data;
+        responseStatus.success = true;
+      })
+      .catch(() => {
+        responseStatus.success = false;
+        responseStatus.message = 'Server error. Unexpected status code.'
+      });
+
+      return responseStatus;
+    },
+  },
+})
