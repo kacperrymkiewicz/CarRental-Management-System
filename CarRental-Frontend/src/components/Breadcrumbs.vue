708915a9abe925eaef03cc9cbae4deef1cc217b9
Kacper <kacperrymkiewicz@gmail.com>
2023-12-03 02:45:24 +0100

    Add breadcrumbs component

---
+++ CarRental-Frontend/src/components/Breadcrumbs.vue
@@ -0,0 +1,38 @@
+<script setup>
+const props = defineProps({
+  isCustomer: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div>
+    <span> {{ isCustomer ? "Panel klienta" : "Panel pracownika" }} </span>
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  font-weight: 600;
+  color: $secondary;
+  text-align: left;
+  margin-top: 30px;
+
+  :slotted(a) {
+    text-decoration: none;
+    color: $secondary;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &::before {
+      content: url("/frontend/images/icons/svg/breadcrumbs_arrow.svg");
+      margin: 0 15px;
+    }
+  }
+}
+</style>
