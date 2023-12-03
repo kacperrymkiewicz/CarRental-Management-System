45276d884264edc9f5134b0f27652624a1a7546d
Kacper <kacperrymkiewicz@gmail.com>
2023-12-03 02:59:36 +0100

    Add WelcomeMessage component

---
+++ CarRental-Frontend/src/components/WelcomeMessage.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="d-flex flex-column align-items-start">
+    <h1 v-if="name">Witaj {{ capitalizeFirstLetter(this.name) }}</h1>
+    <p class="info">
+      <slot name="info"></slot>
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  margin: 25px 0 50px;
+  position: relative;
+
+  h1 {
+    font-size: 28px;
+  }
+
+  p.info {
+    text-align: left;
+  }
+}
+</style>
