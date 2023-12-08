7e9f4eae8c5972db00735c14437c5520b347ebd8
Kacper <kacperrymkiewicz@gmail.com>
2023-12-08 03:21:07 +0100

    Fix axios token reading problem

--- CarRental-Frontend/src/plugins/axios.js
+++ CarRental-Frontend/src/plugins/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://localhost:7233/api';
-axios.defaults.headers.common['Authorization'] = 'bearer ' + localStorage.getItem('authToken');
+axios.defaults.headers.common['Authorization'] = 'bearer ' + JSON.parse(localStorage.getItem('auth'))['authToken'];
