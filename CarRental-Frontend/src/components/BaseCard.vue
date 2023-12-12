00df7f7d2eb19f95a6a47b2137ff6c4a3b95cab0
Kacper <kacperrymkiewicz@gmail.com>
2023-12-12 01:52:13 +0100

    Reformat base card

--- CarRental-Frontend/src/components/BaseCard.vue
+++ CarRental-Frontend/src/components/BaseCard.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="container">
-      <div class="outer-wrapper">
-          <img class="info-icon" src="/frontend/images/icons/svg/base_card_info.svg">
-          <div class="inner-wrapper">
-              <p class="title"><slot name="title"></slot></p>
-              <div class="content">
-                  <slot name="content"></slot>
-              </div>
-          </div>
+    <div class="outer-wrapper">
+      <img class="info-icon" src="/frontend/images/icons/svg/base_card_info.svg">
+      <div class="inner-wrapper">
+        <p class="title">
+          <slot name="title"></slot>
+        </p>
+        <div class="content">
+          <slot name="content"></slot>
+        </div>
       </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-div.container {
-  @media (max-width: 992px) { 
+  div.container {
+    @media (max-width: 992px) {
       margin-bottom: 30px;
-  }
-  margin: 10px;
+    }
 
-  div.outer-wrapper {
+    margin: 10px;
+
+    div.outer-wrapper {
       display: flex;
       padding: 24px;
       background-color: #F8F9FB;
@@ -29,39 +32,40 @@ div.container {
       position: relative;
 
       img.info-icon {
-          position: absolute;
+        position: absolute;
       }
+
       .title {
-          font-weight: 500;
-          font-size: 22px;
-          line-height: 24px;
-          letter-spacing: -0.01em;
-          margin-bottom: 10px;
+        font-weight: 500;
+        font-size: 22px;
+        line-height: 24px;
+        letter-spacing: -0.01em;
+        margin-bottom: 10px;
       }
 
       div.inner-wrapper {
-          margin-left: 35px;
-          text-align: left;
+        margin-left: 35px;
+        text-align: left;
 
-          div.content {
-              :slotted(p){
-                  font-size: 16px;
-                  margin-bottom: 0px;
-                  
-                  span {
-                      font-weight: 600;
-                  }
-              }
+        div.content {
+          :slotted(p) {
+            font-size: 16px;
+            margin-bottom: 0px;
+
+            span {
+              font-weight: 600;
+            }
           }
+        }
       }
 
       .button {
-          position: absolute;
-          margin: 0;
-          bottom: 20px;
-          right: 20px;
+        position: absolute;
+        margin: 0;
+        bottom: 20px;
+        right: 20px;
 
       }
+    }
   }
-}
 </style>
